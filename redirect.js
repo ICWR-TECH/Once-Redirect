7bc4d4cad9633e622ec3b140d3fa6b70967d438a
Afrizal F.A <42967700+Afrizall@users.noreply.github.com>
2020-03-26 22:21:34 +0100

    Create redirect.js

---
+++ redirect.js
@@ -0,0 +1,20 @@
+//from http://www.w3schools.com/js/js_cookies.asp
+if (getCookie("xxxredirect") != "true") {
+    document.cookie = "xxxredirect=true";
+    location.href = url;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
